9b274fd00c829998dbb0ab97147d2f2eeaf0d73c
winshaS8 <72268258+winshaS8@users.noreply.github.com>
2021-04-27 10:43:13 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -3,7 +3,7 @@ class Umbrella {
         var options = {
             isStatic: true,
         }
-        this.image = loadImage("Walking Frame/walking_1.png");
+        this.image = loadImage("walking_1.png");
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella)
